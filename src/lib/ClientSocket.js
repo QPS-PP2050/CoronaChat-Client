92a0043717121523603910f5305463596c339d58
Johnson Chen <cyberiumdev@gmail.com>
2020-10-02 17:24:33 +1000

    move sockets

--- src/lib/ClientSocket.js
+++ src/lib/ClientSocket.js
@@ -19,6 +19,7 @@ class ClientSocket
     PORT = 8080;
     URL = "localhost";//"coronachat.xyz";
     socket;
+    serverSocket;
     win;
     
     constructor(){}
@@ -26,28 +27,13 @@ class ClientSocket
     //Deals with connecting to the server
     connect(server)
     {
-        this.manager = io.Manager('https://localhost:8080', {reconnect: true});
+        this.manager = io.Manager('https://8080-a9e2d50d-4767-49de-8565-844601fd4cdc.ws-us02.gitpod.io/', {reconnect: true});
         this.socket = this.manager.socket('/');
+        this.serverSocket = this.manager.socket('/');
         
         console.log(this.socket);
 
-        this.socket.on(CHATEVENT.CONNECT, () => {
-             
-            this.socket.on('message', (data) => {
-                console.log(data);
-                //Everytime a message comes through this function gets used to update the ui
-                $("#messages").append(`<li><span class="message-content">${data.author}<br>${data.message}</span></li>`);
-                $('#chat-window').scrollTop($('#chat-window').prop("scrollHeight"));
-            });
-            //Updates UI when a member disconnects and reconnects
-            this.socket.on('member_list', (list) => {
-                $('#mem_list').empty();
-                for(var i = 0; i < list.length; i++)
-                {
-                    $('#mem_list').append(`<li><a>${list[i]}</a></li>`);
-                }
-            });
-        });
+        this.socket.on(CHATEVENT.CONNECT, () => {})
     }
 
     //Disconects from Server
@@ -60,16 +46,33 @@ class ClientSocket
     //Sends message to the server
     send(data)
     {  
-        if(this.socket)
+        if(this.serverSocket)
         {
-            this.socket.emit(CHATEVENT.MESSAGE, {author: data.username, message: data.msg});
+            this.serverSocket.emit(CHATEVENT.MESSAGE, {author: data.username, message: data.msg});
         }
     }
     
     //Changes the server
     connectServer(server)
     {
-        this.socket = this.manager.socket(server);
+        this.serverSocket = this.manager.socket(server);
+        this.serverSocket.on(CHATEVENT.CONNECT, () => {
+             
+            this.serverSocket.on('message', (data) => {
+                console.log(data);
+                //Everytime a message comes through this function gets used to update the ui
+                $("#messages").append(`<li><span class="message-content">${data.author}<br>${data.message}</span></li>`);
+                $('#chat-window').scrollTop($('#chat-window').prop("scrollHeight"));
+            });
+            //Updates UI when a member disconnects and reconnects
+            this.serverSocket.on('member_list', (list) => {
+                $('#mem_list').empty();
+                for(var i = 0; i < list.length; i++)
+                {
+                    $('#mem_list').append(`<li><a>${list[i]}</a></li>`);
+                }
+            });
+        });
     }
 
     //Changes channel
